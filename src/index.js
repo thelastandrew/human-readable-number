6e37cd246f3cda9117601cb990a63a8dc6e747ad
thelastandrew <thelastandrew@gmail.com>
2022-02-05 10:06:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const units = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const dozens = [
+        null,
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    if (number < 20) {
+        return units[number];
+    } else if (number >= 20 && number <= 99) {
+        if (number % 10 === 0) {
+            return dozens[number / 10];
+        } else {
+            return dozens[Math.floor(number / 10)] + " " + units[number % 10];
+        }
+    } else {
+        if (number >= 100 && number <= 999) {
+            if (number % 100 === 0) {
+                return units[number / 100] + " hundred";
+            } else if (number % 10 === 0) {
+                return (
+                    units[Math.floor(number / 100)] +
+                    " hundred " +
+                    dozens[(number % 100) / 10]
+                );
+            } else if (number % 100 >= 1 && number % 100 <= 19) {
+                return (
+                    units[Math.floor(number / 100)] +
+                    " hundred " +
+                    units[number % 100]
+                );
+            } else {
+                return (
+                    units[Math.floor(number / 100)] +
+                    " hundred " +
+                    dozens[Math.floor((number % 100) / 10)] +
+                    " " +
+                    units[(number % 100) % 10]
+                );
+            }
+        }
+    }
+};
